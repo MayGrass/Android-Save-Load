8cb238824efd53bdd88a5cf93bce67b56b9e02d4
DCH <dching0121@gmail.com>
2019-11-15 14:14:54 +0800

    匯出檔案

--- app/src/main/java/tw/org/iii/saveloadfile/MainActivity.java
+++ app/src/main/java/tw/org/iii/saveloadfile/MainActivity.java
@@ -8,6 +8,10 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
     private SharedPreferences sp;
     private SharedPreferences.Editor editor;
@@ -21,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void test1(View view) {
+        //存資料data/data/appname/shared_preds
         editor.putString("username", "DCH");
         editor.putBoolean("sound", false);
         editor.putInt("stage", 4);
@@ -33,5 +38,21 @@ public class MainActivity extends AppCompatActivity {
         String username = sp.getString("username", "nobody");
         int stage = sp.getInt("stage", 1);
         Log.v("DCH", username + ":" + stage + ":" + isSound);
+    }
+
+    public void test3(View view) {
+        try {
+            //資料存在data/data/appname/files裡
+            FileOutputStream fout = openFileOutput("DCH.txt", MODE_APPEND); //創檔名DCH.txt (不一定需要副檔名)
+            fout.write("Hello World\n".getBytes()); //字串轉Byte
+            fout.flush();
+            fout.close();
+            Toast.makeText(this, "Save OK", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            Log.v("DCH", e.toString());
+        }
+    }
+
+    public void test4(View view) {
     }
 }
